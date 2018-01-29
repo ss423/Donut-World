af107f531cde850c5b64c7953d115c05e7c47aa5
theresaveit <tv021@hdm-stuttgart.de>
2018-01-29 17:03:15 +0100

    Style Nutzererstellung

--- system/account/nutzererstellung_erfolgreich.php
+++ system/account/nutzererstellung_erfolgreich.php
@@ -1,8 +1,10 @@
 <?php
 echo"
+<br><br>
+
 <div class='row'>
 <div class='col-sm-12'>
-        <h3>Die Registrierung war erfolgreich.</h3>
+        <h3 style='text-align: center;'>Die Registrierung war erfolgreich.</h3>
 </div>
 </div>
 ";
