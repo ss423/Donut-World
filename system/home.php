273f36af4dd1afe0d7b3213253f56f7b24a9475e
ss423 <1996spieth@gmail.com>
2018-01-30 14:40:15 +0100

    homeseite

--- system/home.php
+++ system/home.php
@@ -43,20 +43,24 @@ echo "
 
 <div class='row '>
     <div class='col-sm-6 umrandung_eins'>
-        <div class='col-sm-4'>
-        <br>
-            <img class='img-responsive homebild' src='bilder/home2.jpg' width='100%' title='Neueröffnung' alt='Neueröffnung Donut World' >
+        <div class='row'>
+            <img class='img-responsive homebild' src='bilder/home9.jpg' width='100%' title='Neueröffnung' alt='Neueröffnung Donut World' >
         </div>
-        <div class='col-sm-8'>
+        <div class='row'>
+        <div class='col-sm-1'>
+        </div>
+        <div class='col-sm-10'>
             <h3>NEU IN DER STADT</h3>
-            Willkomen bei Donut World!<br>
             Nach nur vier Monaten in der Online Welt und vielen kleinen Aufträgen, eröffnen wir nun 
             endlich unser erster Donut Laden im Fluxus in Stuttgart.
             Wir werden nun für die kommenden 6 Monaten den besten Platz in der Stadt Stuttgart haben.
-            Nun haben wir die Chance euch von unseren tollen Donuts zu überzeugen!<br>
-            Also schaut vorbei und probiert die kleinen Köstlichkeiten.
+            Nun haben wir die Chance euch von unseren tollen Donuts zu überzeugen!
+            Also schaut vorbei und probiert die kleinen Köstlichkeiten.<br>
             Wir freuen uns auf euch!<br>
-            Euer Donut World Team<br>
+            Euer Donut World Team
+        </div>
+        <div class='col-sm-1'>
+        </div>
         </div>
     </div>
     
@@ -64,17 +68,23 @@ echo "
         </div>
         
     <div class='col-sm-5 umrandung_eins'>
-         <div class='col-sm-4'>
-         <br>
+         <div class='row'>
             <img class='img-responsive homebild' src='bilder/sechs.jpg' width='100%' title='Special' alt='Spezial Donut World'>
         </div>
-        <div class='col-sm-8'>
+        <div class='row'>
+         <div class='col-sm-1'>
+        </div>
+        <div class='col-sm-10'>
             <h3>SPECIALS</h3>
-            Kauft 5 Donuts und erhaltet 6!!!<br>
-            Ab morgen den 12.12.2017 startet unsere Winter-Aktion!
-            Dazu müsst ihr für 6 DOnuts nur 5 Donuts bezahlen, dass bedeutet ihr bekommt einfach einen Donut geschenkt.
-            Das ist ein kleines Weihnachtsgeschenk an euch!<br>
-            Euer Donut World Team wünscht euch eine schöne Weihnachtszeit!<br>
+            Kauft 5 Donuts - erhaltet 6!!!<br>
+            Ab morgen den 02.02.2018 startet unsere Valentins-Aktion!
+            Kommt dazu einfach bei uns im Store vorbei oder schaut in unserem neuen Online-Store, sucht euch sechs Donuts aus und zahl nur fünf Donuts!
+            Das heißt ihr bekommt einen Donut Gratis!!!
+            Der sechste Donut ist ein kleines Valentinsgeschenk an euch!<br>
+            Euer Donut World Team wünscht euch eine schöne Valentinszeit!<br>
+        </div>
+         <div class='col-sm-1'>
+        </div>
         </div>
     </div>
 </div>
@@ -83,11 +93,13 @@ echo "
 
 <div class='row'>
     <div class='col-sm-5 umrandung_zwei'>
-        <div class='col-sm-4'>
-        <br>
-           <img class='img-responsive homebild' src='bilder/home7.jpg' width='100%' title='Magazin' alt='Donut World Magazin'>
+        <div class='row'>
+           <img class='img-responsive homebild' src='bilder/home1.jpg' width='100%' title='Magazin' alt='Donut World Magazin'>
         </div>
-        <div class='col-sm-8'>
+        <div class='row'>
+         <div class='col-sm-1'>
+        </div>
+        <div class='col-sm-10'>
             <h3>PRESSE</h3>
             Was 1 Naicer Donut-Spiegel Magazin<br>
             Was ist den das? So ein kleines Start Up Unternehmen hat schon ein Naicen Magazin?!<br>
@@ -96,16 +108,21 @@ echo "
              Na, Interesse?! Dann einfach in den nächsten Laden gehen und ein Naices Donut World Magazin kaufen!<br>
              Euer Donut World Team<br>
         </div>
+         <div class='col-sm-1'>
+        </div>
+        </div>
     </div>
 
     <div class='col-sm-1'></div>
 
     <div class='col-sm-6 umrandung_zwei'>
-        <div class='col-sm-4'>
-        <br>
-            <img class='img-responsive homebild' src='bilder/love.jpg' width='100%' title='Liebesdonut' alt='Donut World Liebes Donut'>
+        <div class='row'>
+            <img class='img-responsive homebild' src='bilder/valentinstag.jpg' width='100%' title='Liebesdonut' alt='Donut World Liebes Donut'>
         </div>
-        <div class='col-sm-8'>
+        <div class='row'>
+         <div class='col-sm-1'>
+        </div>
+        <div class='col-sm-10'>
             <h3>DONUT FÜR DIE LIEBSTEN</h3>
             Es ist mal wieder Februar..und was heißt das? Genau, es ist wieder <b>Valentinstag!</b> 
             Der Tag an dem man seinen Liebsten mit von Herzen kommenden Geschenk zeigt, dass sie einem wichtig sind und, dass man sie
@@ -116,6 +133,9 @@ echo "
             zergeht. <br>
             Na, Lust auf ein Geschmacksorgasmus bekommen? Dann schau schnell im <a href='index.php?page=alledonuts'>Online-Shop</a> vorbei, 
             denn die Auswahl ist begrenzt! 
+        </div>
+         <div class='col-sm-1'>
+        </div>
         </div>
     </div>
 </div>
