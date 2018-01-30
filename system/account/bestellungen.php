d1b2f3fb6f1a1bc03eea9a549e250d4a21ade658
ss423 <1996spieth@gmail.com>
2018-01-30 17:50:20 +0100

    bestellung nutzer

--- system/account/bestellungen.php
+++ system/account/bestellungen.php
@@ -5,7 +5,7 @@ $nachname=$_SESSION["nutzer"]["nachname"]; //rauslesen Nutzer Nachname
 
 echo"
 <div class='row'>
-    <div class='col-sm-12 ueberschrift'>
+    <div class='col-sm-12 produkte'>
         <h1>Meine Seite</h1>
     </div>
 </div>
@@ -18,49 +18,90 @@ echo"
 
 echo "
 <div class='row'>
-    <div class='col-sm-1'>
+    <div class='col-sm-4'>
         Name:<br>
         Adresse:<br>
     </div>
-    <div class='col-sm-11'>";    //Nutzerangaben (Adresse)
-        echo $_SESSION["nutzer"]["vorname"] . " " .$_SESSION["nutzer"]["nachname"]. "<br>";
-        echo $_SESSION["nutzer"]["straße"] . " " .$_SESSION["nutzer"]["hausnummer"]. "<br>" . $_SESSION["nutzer"]["plz"]. " " . $_SESSION["nutzer"]["ort"];
-        echo "</div></div>";
-        $id=$_SESSION["nutzer"]["id"];
-
+    <div class='col-sm-8'>";    //Nutzerangaben (Adresse)
+echo $_SESSION["nutzer"]["vorname"] . " " .$_SESSION["nutzer"]["nachname"]. "<br>";
+echo $_SESSION["nutzer"]["straße"] . " " .$_SESSION["nutzer"]["hausnummer"]. "<br>" . $_SESSION["nutzer"]["plz"]. " " . $_SESSION["nutzer"]["ort"];
+echo "</div></div>";
+$id=$_SESSION["nutzer"]["id"];
+echo $id;
 echo"
-<br>
 <h2>Meine Bestellungen</h2>
+<div class='row'>
+<div class='col-sm-12 strich'>
+</div>
+</div>
+<div class='row'>
+<br>
+</div>
 ";
 
 //rauslesen Bestellte Artikel
-$stmt = $db->prepare("SELECT * FROM bestellungen b, bestellte_artikel ba, artikel a 
-WHERE b.benutzer_id=".$id." AND b.id=ba.bestellungen_id AND ba.artikel_id=a.id ORDER BY ba.bestellungen_id");
-
-if(!$stmt->execute()) {
+$stmt = $db->prepare("SELECT * FROM bestellungen WHERE bestellungen.benutzer_id='".$id."'");
+if(!$stmt->execute()){
     echo "Datenbank-Fehler ";
     $arr = $stmt->errorInfo();
     print_r($arr);
     die();
 }
+while ($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+    $bestellung_id=$row['id'];
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "
+    $stmt2 = $db->prepare("SELECT * FROM bestellte_artikel ba LEFT JOIN artikel ON ba.artikel_id = artikel.id WHERE ba.bestellungen_id='".$bestellung_id."'");
+    if(!$stmt2->execute()){
+        echo "Datenbank-Fehler ";
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+        die();
+    }
+    echo "Sie haben folgende Produkt/-e am ".$row["erstellt"]." bestellt:";
+    while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+        echo "
+        <div class='row'>
+        <br>
+        </div>
     <div class='row'>
         <div class='col-sm-4'>
-            <img class='img-responsive' src='bilder/" . $row["ean"] . ".jpg' width='40%' alt='Bild: ".$row['donutname']."' title='".$row['donutname']."'>
+            <img class='img-responsive' src='bilder/" . $row2["ean"] . ".jpg' width='40%' alt='Bild: ".$row2['donutname']."' title='".$row['donutname']."'>
         </div>
         <div class='col-sm-8'>
             ";
-            $id = $row["id"];
-            echo "<h3>" . $row["donutname"] . "</h3>";
-            echo "Einzelpreis: " . $row["preis"] . "<br>";
-            echo "EAN-Code: " . $row["ean"]."<br>";
-            echo "Menge: " . $row["menge"]."<br>";
-            echo "Bestellt am: ".$row["erstellt"];
+        $id = $row2["id"];
+        echo "<h3>" . $row2["donutname"] . "</h3>";
+        echo "Einzelpreis: " . $row2["preis"] . " Euro<br>";
+        echo "EAN-Code: " . $row2["ean"]."<br>";
+        echo "Menge: " . $row2["menge"]."<br>";
         echo "</div>
     </div>
-    <div class='row strich'>
-    </div>
     ";
+    }
+    echo "<div class='row'>
+        <br>
+        </div>
+
+            <div class='row'>
+            <div class='col-sm-4'>
+    
+            </div>
+            <div class='col-sm-8'>
+    ";
+    echo "Endpreis: ".$row['endpreis']." Euro<br><br>
+            </div>
+            </div>
+    ";
+    echo"<div class='row'>
+            <div class='col-sm-12 strich'>
+            </div>
+        </div>
+        <div class='row'>
+        <br>
+        </div>
+";
+
+
 }
+/*
+*/
