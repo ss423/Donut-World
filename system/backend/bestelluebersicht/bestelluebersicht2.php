5614d49bd6e7ba1cd652520873b8a2374b3b46ab
ss423 <1996spieth@gmail.com>
2018-01-30 18:15:05 +0100

    bestellübersicht backend

--- system/backend/bestelluebersicht/bestelluebersicht2.php
+++ system/backend/bestelluebersicht/bestelluebersicht2.php
@@ -12,10 +12,21 @@ echo "
         </div>
     </div>
 </div>
-<br>";
+<br>
+   <div class='row'>
+        <div class='col-sm-12 strich'>
+        </div>
+    </div>
+    <div class='row'>
+        <br>
+    </div>
+
+";
 
 
-$stmt = $db->prepare("SELECT * FROM bestellungen b, bestellte_artikel ba, benutzer be WHERE b.id=ba.bestellungen_id AND b.benutzer_id=be.id ORDER BY b.erstellt");
+$stmt = $db->prepare("SELECT bestellungen.id, bestellungen.endpreis, bestellungen.erstellt, benutzer.nachname, benutzer.vorname, benutzer.straße, benutzer.hausnummer, benutzer.ort, benutzer.plz 
+FROM bestellungen LEFT JOIN benutzer 
+ON bestellungen.benutzer_id=benutzer.id ORDER BY bestellungen.erstellt");
 
 if(!$stmt->execute()) {
     echo "Datenbank-Fehler ";
@@ -26,56 +37,76 @@ if(!$stmt->execute()) {
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo "
+
     <div class='row'>
         <div class='col-sm-12'>
-        <h3>Bestellungsnummer: ".$row["bestellungen_id"]."</h3><br>
+        <h3>Bestellungsnummer: " . $row["id"] . "</h3><br>
         </div>
     </div>
     <div class='row'>
         <div class='col-sm-4'>";
-    echo "<h4>Name: </h4>".$row["nachname"].", ".$row["vorname"]."<br>
+    echo "<h4>Name: </h4>" . $row["nachname"] . ", " . $row["vorname"] . "<br>
         </div>
         <div class='col-sm-4'>
     ";
-    echo "<h4>Straße: </h4>".$row["straße"]." ".$row["hausnummer"]."<br>
+    echo "<h4>Straße: </h4>" . $row["straße"] . " " . $row["hausnummer"] . "<br>
         </div>
         <div class='col-sm-4'>
-        <h4>Bestellt am:</h4>" . $row["erstellt"]."
+        <h4>Bestellt am:</h4>" . $row["erstellt"] . "
         </div>
     </div><br>";
-    $bestellungid= $row["bestellungen_id"];
+    $bestellung_id = $row["id"];
 
-    $stmt2 = $db->prepare("SELECT * FROM bestellte_artikel ba LEFT JOIN artikel a ON ba.artikel_id = a.id WHERE ba.bestellungen_id='".$bestellungid."'");
 
+    $stmt2 = $db->prepare("SELECT * FROM bestellte_artikel ba LEFT JOIN artikel ON ba.artikel_id = artikel.id WHERE ba.bestellungen_id='" . $bestellung_id . "'");
     if (!$stmt2->execute()) {
         echo "Datenbank-Fehler ";
-        $arr = $stmt2->errorInfo();
+        $arr = $stmt->errorInfo();
         print_r($arr);
         die();
     }
-
     while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
         echo "
-<div class='row'>
-    <div class='col-sm-4'>
-        <img class='img-responsive' src='../../bilder/" . $row2["ean"] . ".".$row2["ende"]."' width='40%' alt='Bild: ".$row2['donutname']."' title='".$row2['donutname']."'>
+    <div class='row'>
+        <br>
     </div>
-    <div class='col-sm-8'>
-    ";
+    <div class='row'>
+        <div class='col-sm-4'>
+            <img class='img-responsive' src='../../bilder/" . $row2["ean"] . ".jpg' width='40%' alt='Bild: ".$row2['donutname']."' title='".$row['donutname']."'>
+        </div>
+        <div class='col-sm-8'>
+            ";
         $id = $row2["id"];
         echo "<h3>" . $row2["donutname"] . "</h3>";
-        echo "Einzelpreis: " . $row2["preis"] . "<br>";
-        echo "EAN-Code: " . $row2["ean"] . "<br>";
-        echo "Menge: " . $row2["menge"] . "<br><br>";
+        echo "Einzelpreis: " . $row2["preis"] . " Euro<br>";
+        echo "EAN-Code: " . $row2["ean"]."<br>";
+        echo "Menge: " . $row2["menge"]."<br>";
         echo "</div>
-</div>";
+    </div>
+    ";
     }
-    echo "<div class='strich'>
-        </div><br>";
+    echo "<div class='row'>
+        <br>
+        </div>
+        <div class='row'>
+            <div class='col-sm-4'>
+            </div>
+            <div class='col-sm-8'>
+    ";
+    echo "Endpreis: ".$row['endpreis']." Euro<br><br>
+            </div>
+        </div>
+    ";
+    echo"<div class='row'>
+            <div class='col-sm-12 strich'>
+            </div>
+        </div>
+        <div class='row'>
+            <br>
+        </div>
+";
 }
 echo"    
-    </div>
-</div>
-</div>
+
 ";
 ?>
